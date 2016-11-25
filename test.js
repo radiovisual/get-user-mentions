caba173b079fe8108b380e718be3b03d17cc2a00
Michael Wuergler <wuergler@gmail.com>
2016-11-25 21:56:42 +0000

    lint test file

--- test.js
+++ test.js
@@ -22,7 +22,7 @@ test('finds all user mentions by default', t => {
 });
 
 test('finds unique user mentions', t => {
-	const users = fn('hello @michael and @michael and @mark, how are you?', {unique:true});
+	const users = fn('hello @michael and @michael and @mark, how are you?', {unique: true});
 	t.is(users.length, 2);
 	t.is(users[0], '@michael');
 	t.is(users[1], '@mark');
@@ -40,14 +40,14 @@ test('finds user mentions in latin-extended character sets', t => {
 });
 
 test('finds unique user mentions, with nameOnly option', t => {
-	const users = fn('hello @michael and @michael and @mark, how are you?', {unique:true, nameOnly:true});
+	const users = fn('hello @michael and @michael and @mark, how are you?', {unique: true, nameOnly: true});
 	t.is(users.length, 2);
 	t.is(users[0], 'michael');
 	t.is(users[1], 'mark');
 });
 
 test('allows nameOnly option', t => {
-	const users = fn('hello @michael and @mark, how are you?', {nameOnly:true});
+	const users = fn('hello @michael and @mark, how are you?', {nameOnly: true});
 	t.is(users.length, 2);
 	t.is(users[0], 'michael');
 	t.is(users[1], 'mark');
